21d7f28840f41744f1d1e3742ee46b9af5fc8bb0
bhavikchotalia <chotaliabhavik123@gmail.com>
2024-05-01 21:20:41 +1000

    Initial Commit

---
+++ ThoughtTrace/ThoughtTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtTraceApp.swift
+//  ThoughtTrace
+//
+//  Created by Bhavik Chotalia on 1/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
