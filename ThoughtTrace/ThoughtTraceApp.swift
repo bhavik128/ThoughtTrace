b27202659b8108349cb8758bc16594e31d165d70
bhavikchotalia <chotaliabhavik123@gmail.com>
2024-05-05 23:19:01 +1000

    added firebase auth

--- ThoughtTrace/ThoughtTraceApp.swift
+++ ThoughtTrace/ThoughtTraceApp.swift
@@ -5,10 +5,24 @@
 //  Created by Bhavik Chotalia on 1/5/2024.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct ThoughtTraceApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
