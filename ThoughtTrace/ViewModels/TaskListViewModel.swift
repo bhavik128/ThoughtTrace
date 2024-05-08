778d8bde0466189226b462a31aa3cf558d47df50
Zareen <57041109+zareensc@users.noreply.github.com>
2024-05-09 00:22:37 +1000

    Made task list scrollable and integrated zenquotes API

---
+++ ThoughtTrace/ViewModels/TaskListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskListViewModel.swift
+//  ThoughtTrace
+//
+//  Created by Zareen Sharar Cynthia on 8/5/2024.
+//
+
+import Foundation
---
+++ ThoughtTrace/ViewModels/TaskViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskViewModel.swift
+//  ThoughtTrace
+//
+//  Created by Zareen Sharar Cynthia on 8/5/2024.
+//
+
+import Foundation
