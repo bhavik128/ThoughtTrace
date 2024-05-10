6a06d2e69c86efbd990335c2b194e282f95678f5
Zareen <57041109+zareensc@users.noreply.github.com>
2024-05-10 16:28:41 +1000

    Revert "Added upcoming tasks component, add task option and view calendar option"
    This reverts commit dfb32efdf8c562ddb73f4e6f413aa814ad88eed1.

--- ThoughtTrace/Views/ContentView.swift
+++ ThoughtTrace/Views/ContentView.swift
@@ -1,86 +1,24 @@
+//
+//  ContentView.swift
+//  ThoughtTrace
+//
+//  Created by Bhavik Chotalia on 1/5/2024.
+//
+
 import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
-    @State private var topTasks = ["Task 1", "Task 2", "Task 3"] 
 
     var body: some View {
-//        Group {
-//            if authViewModel.isAuthenticated {
-
-        
+        Group {
+            if authViewModel.isAuthenticated {
                 VStack {
-                    Text("ThoughtTrace")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.indigo)
-                    Spacer()
-                    
-                    VStack(alignment: .center) {
-                        Text("Upcoming Tasks")
-                            .font(.headline)
-                            .foregroundColor(.indigo)
-                            .bold()
-                        
-                        ForEach(topTasks, id: \.self) { task in
-                            Text(task)
-                                .padding(.vertical, 5)
-                                .frame(minWidth: 200)
-                                .background(.indigo)
-                                .cornerRadius(10)
-                                .foregroundColor(.white)
-                        }
-                        
-                        NavigationLink(destination: TaskListView()) {
-                            Text("See All Tasks")
-                                .bold()
-                                .foregroundColor(.indigo)
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
 
-                        }
-                    }
-                    .padding()
-                    .frame(width: 300)
-                    .background(.indigo.opacity(0.3))
-                    .cornerRadius(25)
-                    Spacer()
-                    
-                    HStack {
-                        VStack {
-                            NavigationLink(destination: AddTaskView()) {
-                                Text("Add New Task")
-                                    .font(.title2)
-                                    .foregroundColor(.purple)
-                                    .bold()
-                            }
-                            
-                            
-                            
-                            Image("task")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 30, height: 30)
-                        }
-                        
-                        Spacer()
-                        
-                        VStack {
-                            NavigationLink(destination: CalendarView()) {
-                                Text("View Calendar")
-                                    .font(.title2)
-                                    .foregroundColor(.purple)
-                                    .bold()
-                            }
-                            
-                            
-                            Image("calendar")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 30, height: 30)
-                        }
-                    }
-                    .padding()
-                    Spacer()
-                    
                     Button {
                         authViewModel.signOut()
                     } label: {
@@ -88,10 +26,10 @@ struct ContentView: View {
                     }
                 }
                 .padding()
-//            } else {
-//                SignInView()
-//            }
-//        }
+            } else {
+                SignInView()
+            }
+        }
     }
 }
 
