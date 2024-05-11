32c933f81f549783adb508ea2599e30781e7b3e8
Zareen <57041109+zareensc@users.noreply.github.com>
2024-05-11 23:33:32 +1000

    re-added all contentview components

---
+++ ThoughtTrace/Views/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  ThoughtTrace
+//
+//  Created by Zareen Sharar Cynthia on 11/5/2024.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
---
+++ ThoughtTrace/Views/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  ThoughtTrace
+//
+//  Created by Zareen Sharar Cynthia on 11/5/2024.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
